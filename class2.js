b6ba06d48cfc48bb0b51cc7476ba57cd0fb0e838
nonex10 <xenusaud1010@gmail.com>
2024-08-13 10:38:13 +0545

    first commit

---
+++ class2.js
@@ -0,0 +1,4 @@
+function updateBody(){
+  const pTag = document.getElementsByClassName('body');  /* getelembyid: only euta aauxa, classname:list auxa*/
+  pTag[0].innerText = 'this is change from script';
+} 
